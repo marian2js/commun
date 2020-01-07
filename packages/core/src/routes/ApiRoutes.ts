92f73c0467b2bb05a43e2be8d92402acac11a49f
marian2js <marianomdq1@gmail.com>
2020-01-07 13:33:05 -0300

    Delegate errors to the express error handler

--- packages/core/src/routes/ApiRoutes.ts
+++ packages/core/src/routes/ApiRoutes.ts
@@ -1,31 +1,25 @@
 import * as express from 'express'
-import { Request, Response } from 'express'
+import { NextFunction, Request, Response } from 'express'
 import { Commun } from '../Commun'
-import { HttpResponseError } from '../errors/HttpResponseError'
 
-const processResponse = async <T> (req: Request, res: Response, resultPromise: Promise<T>) => {
+const sendResponse = async <T> (req: Request, res: Response, next: NextFunction, resultPromise: Promise<T>) => {
   try {
-    const result = await resultPromise
-    res.send(result)
+    res.send(await resultPromise)
   } catch (e) {
-    if (e instanceof HttpResponseError) {
-      return res.status(e.statusCode).send({ error: e.message })
-    }
-    console.log(e)
-    res.status(500).send('Bad Request')
+    next(e)
   }
 }
 
 const router = express.Router()
-  .get('/:entity', (req, res) =>
-    processResponse(req, res, Commun.getController(req.params.entity).list(req, res)))
-  .post('/:entity', (req, res) =>
-    processResponse(req, res, Commun.getController(req.params.entity).create(req, res)))
-  .get('/:entity/:id', (req, res) =>
-    processResponse(req, res, Commun.getController(req.params.entity).get(req, res)))
-  .put('/:entity/:id', (req, res) =>
-    processResponse(req, res, Commun.getController(req.params.entity).update(req, res)))
-  .delete('/:entity/:id', (req, res) =>
-    processResponse(req, res, Commun.getController(req.params.entity).delete(req, res)))
+  .get('/:entity', (req, res, next) =>
+    sendResponse(req, res, next, Commun.getController(req.params.entity).list(req, res)))
+  .post('/:entity', (req, res, next) =>
+    sendResponse(req, res, next, Commun.getController(req.params.entity).create(req, res)))
+  .get('/:entity/:id', (req, res, next) =>
+    sendResponse(req, res, next, Commun.getController(req.params.entity).get(req, res)))
+  .put('/:entity/:id', (req, res, next) =>
+    sendResponse(req, res, next, Commun.getController(req.params.entity).update(req, res)))
+  .delete('/:entity/:id', (req, res, next) =>
+    sendResponse(req, res, next, Commun.getController(req.params.entity).delete(req, res)))
 
 export default router
