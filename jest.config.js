4d8a84f6c10cbccfd4240f0881252dc39d74d3b2
marian2js <marianomdq1@gmail.com>
2020-01-14 15:04:46 -0300

    Add global jest configuration

---
+++ jest.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  preset: '@shelf/jest-mongodb',
+  globals: {
+    'ts-jest': {
+      tsConfig: 'tsconfig.json'
+    }
+  },
+  moduleFileExtensions: [
+    'ts',
+    'js',
+    'json'
+  ],
+  transform: {
+    '^.+\\.(ts|tsx)$': 'ts-jest'
+  },
+  testMatch: [
+    '**/test/**/*.test.(ts|js)'
+  ],
+  testEnvironment: 'node'
+}
