7f906169a8b1430cd08ebdabd8dbbe2f886dd3d2
marian2js <marianomdq1@gmail.com>
2020-07-17 17:25:22 -0300

    Add GraphQL support for object type attributes

--- packages/graphql/test/SchemaBuilder.test.ts
+++ packages/graphql/test/SchemaBuilder.test.ts
@@ -17,37 +17,63 @@ describe('SchemaBuilder', () => {
 
     it('should convert boolean to GraphQL type', async () => {
       const config = createEntityConfig({ type: 'boolean' })
-      expect(getAttributeGraphQLType(config, 'key', config.attributes.key, 'type')).toEqual(GraphQLBoolean)
+      expect(getAttributeGraphQLType(config, 'key', config.attributes.key, 'type', x => x))
+        .toEqual(GraphQLBoolean)
     })
 
     it('should convert email to GraphQL type', async () => {
       const config = createEntityConfig({ type: 'email' })
-      expect(getAttributeGraphQLType(config, 'key', config.attributes.key, 'type')).toEqual(GraphQLString)
+      expect(getAttributeGraphQLType(config, 'key', config.attributes.key, 'type', x => x))
+        .toEqual(GraphQLString)
     })
 
     it('should convert slug to GraphQL type', async () => {
       const config = createEntityConfig({ type: 'slug', setFrom: 'test' })
-      expect(getAttributeGraphQLType(config, 'key', config.attributes.key, 'type')).toEqual(GraphQLString)
+      expect(getAttributeGraphQLType(config, 'key', config.attributes.key, 'type', x => x))
+        .toEqual(GraphQLString)
     })
 
     it('should convert string to GraphQL type', async () => {
       const config = createEntityConfig({ type: 'string' })
-      expect(getAttributeGraphQLType(config, 'key', config.attributes.key, 'type')).toEqual(GraphQLString)
+      expect(getAttributeGraphQLType(config, 'key', config.attributes.key, 'type', x => x))
+        .toEqual(GraphQLString)
     })
 
     it('should convert date to GraphQL type', async () => {
       const config = createEntityConfig({ type: 'date' })
-      expect(getAttributeGraphQLType(config, 'key', config.attributes.key, 'type')).toEqual(GraphQLDate)
+      expect(getAttributeGraphQLType(config, 'key', config.attributes.key, 'type', x => x))
+        .toEqual(GraphQLDate)
     })
 
     it('should convert id to GraphQL type', async () => {
       const config = createEntityConfig({ type: 'id' })
-      expect(getAttributeGraphQLType(config, 'key', config.attributes.key, 'type')).toEqual(GraphQLID)
+      expect(getAttributeGraphQLType(config, 'key', config.attributes.key, 'type', x => x))
+        .toEqual(GraphQLID)
     })
 
     it('should convert numbers to GraphQL type', async () => {
       const config = createEntityConfig({ type: 'number' })
-      expect(getAttributeGraphQLType(config, 'key', config.attributes.key, 'type')).toEqual(GraphQLFloat)
+      expect(getAttributeGraphQLType(config, 'key', config.attributes.key, 'type', x => x))
+        .toEqual(GraphQLFloat)
+    })
+
+    it('should convert lists to GraphQL type', async () => {
+      const config = createEntityConfig({ type: 'list', listType: { type: 'string' } })
+      expect(
+        getAttributeGraphQLType(config, 'key', config.attributes.key, 'type', x => x).toString())
+        .toEqual('[String]')
+    })
+
+    it('should convert objects to GraphQL type', async () => {
+      const config = createEntityConfig({
+        type: 'object', fields: {
+          a: { type: 'string' },
+          b: { type: 'number' },
+        },
+      })
+      expect(
+        getAttributeGraphQLType(config, 'key', config.attributes.key, 'type', x => x).toString())
+        .toEqual('Key')
     })
   })
 })
